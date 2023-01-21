cfe8ed6af9c0f3b70a86530113bf003b9369f61b
sdorado2 <sdorado2@gmail.com>
2023-01-21 18:11:08 -0500

    initial JS datatypes created.

---
+++ app.js
@@ -0,0 +1,58 @@
+// Strings
+let family='Father';
+let memberNumber='5';
+let picture='Mona Lisa';
+let price='$20,000';
+let vehicle='Toyota';
+let headphone='Audio-Technica';
+let bestShoe='Adidas';
+let tv='LG';
+let drink='Water';
+let computer='Asus';
+
+console.log(typeof memberNumber);
+
+// Interger
+const pi=3.1415;
+let worldCupUSA=1;
+const gravity=9.81;
+let numberOfShoe=3;
+const gConstant=6.64*10**-11;
+let bottleOfWater=40;
+const speedOfLight= 299792458;
+let dozenEggs=12;
+const x=0;
+let applesAday=1; 
+
+console.log(typeof worldCupUSA);
+
+// Array
+const groceryList=['apples','tomatoes','aloe'];
+const phoneVendors=['samsung', 'motorola', 'apple'];
+const smartDevices=['computer', 'laptop','tablet'];
+const trashCompartments=['plastics', 'composite','metals'];
+const rndNumbers=[5,10,15];
+const rndObjects=[10, 'sofa', 300, 'dvd'];
+const wholeNumbers=[1,2,3,4,5];
+const decimalNumbers=[1.1, 2.34, 4.567];
+const colorShirts=['red', 'blue', 'black'];
+const personName=['John', 'Mary'];
+
+console.log(typeof groceryList);
+console.log(typeof rndObjects);
+console.log(typeof decimalNumbers);
+
+// Objects  
+const nameList={firstPerson:'Adam', secondPerson:'Eve'};
+const boroughNYC={north:'Bronx',center:'Manhanttan',south:'Staten Island'};
+const famousStreets={Uptown:'Harlem',midTown:'Times Square',downTown:'Wall Street'};
+const tallTowers={first:'World Trade Center', second:'Empire State Building', third:'Chrysler Tower'};
+const rivers={west:'Hudson River', east:'East River'};
+const computerParts={first:'cpu', second:'motherboard', third:'RAM'};
+const constantSymbolMeaning={g:'Gravity', e:'Speed Of light', G:'Gravitational Constant'};
+const playedGames={first:'Super Mario World', second:'Donkey Kong Country', third:'The Legend Of Zelda'};
+const gamingSystems={one:'Swith', two:'Playstation 5', three:'Xbox series x'};
+const programmingLanguage={markup:'HTML',stylesheet:'CSS', highLevel:'Java'};
+
+console.log(typeof nameList);
+console.log(typeof boroughNYC);
